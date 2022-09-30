fea72609f418782a9e314bf7ad2c486e6b43ae65
mafik_tolik <mafik_tolik@mail.ru>
2022-09-30 16:57:19 +0300

    Ex25

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+
+
+
+// Ex25();
+// Задача 25: Используя определение степени числа, напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+void Ex25()
+{
+    Console.WriteLine("Введите натуральное число A:");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    while (numberA < 1)
+    {
+        Console.WriteLine("Неверный ввод.Введите натуральное число A:");
+        numberA = Convert.ToInt32(Console.ReadLine());
+    }
+
+    Console.WriteLine("Введите натуральное число B:");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    while (numberB < 1)
+    {
+        Console.WriteLine("Неверный ввод.Введите натуральное число B:");
+        numberB = Convert.ToInt32(Console.ReadLine());
+    }
+
+    int result = numberA;
+
+    for (int i = 1; i < numberB; i++)
+    {
+        result *= numberA;
+    }
+    Console.WriteLine($"{numberA} в степени {numberB} = {result}");
+}
+
+
