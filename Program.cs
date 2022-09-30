a0c00e8c61eb191cc6bf86e4931962fe60d1b4d8
mafik_tolik <mafik_tolik@mail.ru>
2022-09-30 18:26:39 +0300

    Ex27();

--- Program.cs
+++ Program.cs
@@ -35,3 +35,52 @@ void Ex25()
 }
 
 
+
+Ex27();
+// Ex27_1(); // решение через массив. 
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+void Ex27()
+{
+    Console.WriteLine("Введите натуральное число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    while (number < 1)
+    {
+        Console.WriteLine("Неверный ввод.Введите натуральное число:");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+
+    int current_number1 = number;
+    int count = 0;
+
+    while (current_number1 > 0)
+    {
+        current_number1 = current_number1 / 10;
+        count += 1;
+    }
+
+    Console.WriteLine($"Количество цифр в числе {number} равно: {count}");
+
+    double sum = 0;
+    double current_number2 = number;
+    int current_count = count - 1;
+
+    for (int i = 0; i < count; i++)
+    {
+
+        double stepen = Math.Pow(10, current_count); // путем деления или умножения на эту переменную мы получим нужный нам разряд числа. 
+
+        double digit = Math.Floor(current_number2 / stepen); // здесь находим первую искомую цифру (начиная слева) и затем последующие.
+
+        sum = sum + digit; // здесь подсчитываем итоговую сумму цифр нашего числа. 
+
+        current_number2 = current_number2 - digit * stepen; // здесь убираем самый высокий разряд (чтобы на следующем цикле работать со следующим разрядом).
+
+        current_count--; // уменьшаем на 1, чтобы на следущем цикле сделать степень на 1 меньше.
+    }
+    Console.WriteLine($"Cумма цифр в числе {number} равна: {sum}");
+}
+
